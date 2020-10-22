557bd5b848d33ae1129a2be3d5afb78de66f6c40
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-10-23 06:17:34 +0900

    文字列

---
+++ var10.php
@@ -0,0 +1,8 @@
+<?php
+$name = "andy";
+
+$message1 = 'hello $name ';
+echo $message1;
+
+$message2 = "hello $name";
+echo $message2;
---
+++ var9.php
@@ -0,0 +1,6 @@
+<?php
+$name = "andy ";
+echo $name;
+
+$name2 = 'betty';
+echo $name2;
